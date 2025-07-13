6fc2421664af8e77b8481b9e5c34b6dcddcd3aa9
Ayaan345 <ayaangaming674@gmail.com>
2025-07-13 16:16:17 +0500

    new fix

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const app = require("./app");
 const serverless = require("serverless-http");
-// module.exports = serverless(app);
-module.exports = (req, res) => {
-  res.json({ message: "Hello from Vercel!" });
-};
+module.exports = serverless(app);
